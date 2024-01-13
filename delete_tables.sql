fb1f88799f2be35edacfee7495115b9f5fdf9287
Diah Junaidi <j.fdiah@gmail.com>
2024-01-13 23:28:13 +0700

    query untuk menghapus table-table

---
+++ delete_tables.sql
@@ -0,0 +1,23 @@
+-- Hapus semua data dari tabel Customer
+DELETE FROM customer;
+
+-- Hapus semua data dari tabel Recipient
+DELETE FROM recipient;
+
+-- Hapus semua data dari tabel Category
+DELETE FROM category;
+
+-- Hapus semua data dari tabel Item
+DELETE FROM item;
+
+-- Hapus semua data dari tabel Payment
+DELETE FROM payment;
+
+-- Hapus semua data dari tabel Orders
+DELETE FROM orders;
+
+-- Hapus semua data dari tabel Order Detail
+DELETE FROM order_detail;
+
+-- Hapus semua data dari tabel Cart
+DELETE FROM cart;
