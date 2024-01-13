ef0b5a48d0f81f9c513563b5fe1b1e344dda6320
Diah Junaidi <j.fdiah@gmail.com>
2024-01-13 23:24:13 +0700

    query untuk melihat data-data fokusnya semua data per table

---
+++ select_data.sql
@@ -0,0 +1,8 @@
+select * from customer c 
+select * from recipient r 
+select * from category c 
+select * from item i 
+select * from payment p 
+select * from orders o 
+select * from order_detail od 
+select * from cart c 
